bd0aa185ad96621270178ba1fc3e05e2de069794
Prasamsa Castelao <pcastelaol22@fpcoruna.afundacion.org>
2023-10-14 20:39:46 +0200

    COnversor2.php del ejercicio 6

--- sprint1apache/www/site3/conversor2.php
+++ sprint1apache/www/site3/conversor2.php
@@ -9,7 +9,11 @@
 		    $v_pulgadas = $_POST["fcantidad"];
 		    $v_metros = $v_pulgadas * 0.0254;
 		    echo $v_pulgadas." pulgada(s) = ".$v_metros." metro(s)";
-		} else{
+		} elseif ($_POST["funidad"] == "pie") {
+		    $v_pies = $_POST["fcantidad"];
+		    $v_metros = $v_pies * 0.3048;
+		    echo $v_pies." pie(s) = ".$v_metros." metro(s)";
+		} else {
 		    echo "Unidad no soportada";
 		}
 	    }
@@ -23,6 +27,9 @@
 	    <input type="radio" id="pulgada_input" name="funidad" value="pulgada">
 	    <label for="pulgada_input">Pulgada(s)</label><br>
 	    
+	    <input type="radio" id="pie_input" name="funidad" value="pie">
+	    <label for="yarda_input">Pie(s)</label><br>
+
 	    <input type="radio" id="otro_input" name="funidad" value="otro">
 	    <label for="otro_input">Otro</label><br>
 
