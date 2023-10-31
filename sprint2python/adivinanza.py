8e9ba87b29ec91f61e11e93b5a0ea11a9d190777
prasamsacl <113896447+prasamsacl@users.noreply.github.com>
2023-10-31 13:49:39 +0100

    Update adivinanza.py

--- sprint2python/adivinanza.py
+++ sprint2python/adivinanza.py
@@ -1,19 +1,16 @@
 # Adivinanza en Python
-
-# Estos son comentarios. No afectan el funcionamiento del programa y se utilizan para agregar notas o explicaciones.
-
 # Pregunta al usuario una adivinanza
 print("Adivinanza:")
 
-# Imprime la adivinanza en la consola.
+# Imprime por consola.
 print("Tengo hojas, pero no soy un libro. Vivo en el jardín, pero no soy una flor. ¿Qué soy?")
 
-# Imprime las opciones para que el usuario pueda elegir.
+# Imprime las opciones para que el usuario pueda elegir la respuesta correcta.
 print("a) Un pájaro")
 print("b) Un árbol")
 print("c) Una flor")
 
-# Obtén la respuesta del usuario
+# Obtener la respuesta del usuario
 #  `input` permite al usuario introducir datos desde la consola y devuelve lo que se ingresa como una cadena.
 respuesta = input("Elige una opción:")
 
