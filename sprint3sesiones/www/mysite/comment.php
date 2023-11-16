57b65a2cf55b6da68e028bcc59563c85ba70c65b
Prasamsa Castelao <pcastelaol22@fpcoruna.afundacion.org>
2023-11-16 12:38:01 +0100

    Ejercicio 4 cambio comment.php

--- sprint3sesiones/www/mysite/comment.php
+++ sprint3sesiones/www/mysite/comment.php
@@ -4,9 +4,16 @@
 <html>
  <body>
 <?php
+//ejercico 4
+//se usa para  crear una sesión nueva
+session_start();
+$user_id_a_insertar = 'NULL';
+if (!empty($_SESSION['user_id'])) {
+$user_id_a_insertar = $_SESSION['user_id'];
+}
   $id = $_POST['id'];
  $comentario = $_POST['new_comment'];
- $query = "INSERT INTO tComentarios(comentario, usuario_id,pelicula_id) VALUES ('".$comentario."',NULL,".$id.")";
+ $query = "INSERT INTO tComentarios(comentario, usuario_id,pelicula_id) VALUES ('".$comentario."',NULL,".$id.",".$user_id_a_insertar.")";
    mysqli_query($db, $query) or die('Error');
  echo "<p>Nuevo comentario ";
  echo mysqli_insert_id($db);
